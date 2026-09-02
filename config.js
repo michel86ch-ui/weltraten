948a53a63023973c2a5367debc40f66c59346a0e
Michel <michel@example.com>
2026-09-02 11:25:13 +0000

    Google Maps API Key eintragen

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // WICHTIG: Diesen Key unter "Anwendungseinschränkungen" auf HTTP-Referrer
 // beschränken (z. B. https://<dein-username>.github.io/*), sonst kann ihn
 // jeder aus dem öffentlichen Quellcode kopieren und dein Budget belasten.
-const GOOGLE_MAPS_API_KEY = "DEIN_API_KEY_HIER";
+const GOOGLE_MAPS_API_KEY = "AIzaSyCxMhVM3HgiRn9pRqq6yBuF3vezcLsW0Pg";
